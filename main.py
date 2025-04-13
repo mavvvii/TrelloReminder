b84aec69a5e0945731964cb439e47f6ddc4d06ab
mavvvii <mavi.steampl@gmail.com>
2025-04-13 21:00:12 +0200

    add-check-tasks-deadline

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+from time import sleep
 import requests
 import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from typing import Final
+from datetime import datetime, timezone, time, timedelta
 
 load_dotenv()
 
 
 class DiscordBot:
-    def __init__(self):
+    def __init__(self) -> None:
         self.DISCORD_API_TOKEN: Final[str] = os.getenv('Discord_API_Token')
 
 
 class TrelloReminder:
-    def __init__(self):
+    def __init__(self) -> None:
         self.TRELLO_API_KEY: Final[str] = os.getenv('Trello_API_Key')
         self.TRELLO_API_TOKEN: Final[str] = os.getenv('Trello_API_Token')
 
@@ -23,7 +25,13 @@ class TrelloReminder:
         self.project_users: list[dict[str, str]] = []
         self.project_tasks: list[dict[str, str]] = []
 
-    def get_members_id_name(self) -> None:
+        self.time_alerts: dict[datetime.timedelta, str] = {
+            timedelta(days=3): 'You have 3 days left!',
+            timedelta(days=1): 'You have 1 day left!',
+            timedelta(hours=1): 'You have 1 hour left - dont forget to check off the task!',
+        }
+
+    def get_members(self) -> None:
         headers: dict[str, str] = {
             "Accept": "application/json"
         }
@@ -44,7 +52,7 @@ class TrelloReminder:
         for item in response.json():
             self.project_users.append(item)
 
-    def get_members_tasks_date(self) -> None:
+    def get_cards_tasks(self) -> None:
         query: dict[str, str] = {
             'fields': 'name,desc,due,idMembers',
             'key': self.TRELLO_API_KEY,
@@ -66,15 +74,30 @@ class TrelloReminder:
                         item['due'] = new_due
             self.project_tasks.append(item)
 
-def main():
-    client.run(token=token_dc)
+    def sent_discord_alert(self, id_members: list[str], message: str) -> None:
+        print(message)
+
+    def check_tasks_deadline(self):
+        for task in self.project_tasks:
+            if task['due'] is not None:
+                date_now: datetime.datetime = datetime.now(timezone.utc)
+
+                task_due_date: datetime.datetime = datetime.fromisoformat(
+                    task['due'].replace("Z", "+00:00")
+                )
+
+                task_due_date_left: datetime.timedelta = task_due_date - date_now
+
+                for alert_time, message in self.time_alerts.items():
+                    if task_due_date_left <= alert_time:
+                        return self.sent_discord_alert(task['idMembers'], message)
 
 if __name__ == "__main__":
-    TrelloReminder = TrelloReminder()
-    TrelloReminder.get_members_id_name()
-    TrelloReminder.get_members_tasks_date()
-    print(TrelloReminder.project_tasks)
-    # print(TrelloReminder.project_tasks)
-    # TrelloReminder.convert_utc_to_cest_time()
-    # print(TrelloReminder.project_users)
-    # print(TrelloReminder.get_members_tasks_date())
+    TrelloReminder: TrelloReminder = TrelloReminder()
+
+    TrelloReminder.get_cards_tasks()
+    TrelloReminder.get_members()
+
+    while True:
+        TrelloReminder.check_tasks_deadline()
+        sleep(60)
