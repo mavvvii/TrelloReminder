82253652957b8f4b268d8411bce36bdbc74e7c21
mavvvii <mavi.steampl@gmail.com>
2025-04-20 13:54:29 +0200

    feat: add global config

---
+++ config.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    trello_api_key: str
+    trello_api_token: str
+    trello_board_id: str
+    discord_api_token: str
+    discord_channel_id: str
+    check_interval: int = 60
+
+    class Config:
+        env_file = ".env"
